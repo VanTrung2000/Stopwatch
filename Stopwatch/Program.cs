487884729bcf6808b618fb25f34590cd06f0ea99
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-22 21:34:37 +0700

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datetime = new DateTime();
+            Stopwatch st = new Stopwatch();
+            int[] arr = new int[10];
+
+            st.InputArray(ref arr);
+            Console.WriteLine(st.Start());
+            st.SortArray(ref arr);
+            st.ShowArray(arr);
+
+            Console.WriteLine(st.End());
+            Console.WriteLine(st.GetElapsedTime());
+        }
+    }
+}
